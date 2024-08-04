e656707e0b88d18ca84275774e559cc7c0b926d6
itsnoctural <nocturalthe@gmail.com>
2024-08-04 19:28:51 +0300

    feat(web): add speed insights

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Background } from "@/components/background";
 import { ClientProviders } from "@/components/client-providers";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { GeistSans } from "geist/font/sans";
 import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
@@ -26,6 +27,7 @@ export default function RootLayout({
             <Background />
             {children}
             <Analytics />
+            <SpeedInsights />
           </ThemeProvider>
         </ClientProviders>
       </body>
