12f60419de60fd6d7a5cd2ab7574e0ce880cd48e
itsnoctural <85783226+itsnoctural@users.noreply.github.com>
2024-08-13 19:29:17 +0300

    feat(web): temp middleware for google crawler

---
+++ apps/web/src/middleware.ts
@@ -0,0 +1,23 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import { api } from "./lib/api";
+
+export async function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+
+  if (pathname.length > 2) {
+    const { status } = await api.v1.scripts({ id: pathname.slice(1) }).get();
+
+    if (status === 200)
+      return NextResponse.redirect(
+        new URL(`/script${request.nextUrl.pathname}`, request.url),
+      );
+  }
+
+  NextResponse.next();
+}
+
+export const config = {
+  matcher:
+    "/((?!_next/static|_next/image|favicon.ico|signin|tos|privacy|script|upload).*)",
+};
