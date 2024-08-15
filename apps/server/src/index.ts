861a009feaaf90b3e5aae1ee0fb50e4c6890a0aa
itsnoctural <85783226+itsnoctural@users.noreply.github.com>
2024-08-15 21:52:42 +0300

    chore(api): remove swagger for now

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,16 +1,7 @@
 import cors from "@elysiajs/cors";
-import swagger from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 import { api as v1 } from "./v1";
 
-export const app = new Elysia()
-  .use((app) => {
-    if (Bun.env.NODE_ENV !== "production") return app.use(swagger());
-
-    return app;
-  })
-  .use(cors())
-  .use(v1)
-  .listen(3000);
+const app = new Elysia().use(cors()).use(v1).listen(3000);
 
 export type App = typeof app;
