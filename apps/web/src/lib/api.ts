668d40dbbe434b102a9921f95ac2cd769b7ee5dc
itsnoctural <85783226+itsnoctural@users.noreply.github.com>
2024-08-30 20:18:40 +0300

    fix(web): server instance on client side

--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -1,4 +1,10 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "@esohasl/server";
 
-export const api = treaty<App>(process.env.NEXT_PUBLIC_API);
+// export const api = treaty<App>(process.env.NEXT_PUBLIC_API);
+
+function initApi() {
+  return treaty<App>(process.env.NEXT_PUBLIC_API);
+}
+
+export const api = initApi();
