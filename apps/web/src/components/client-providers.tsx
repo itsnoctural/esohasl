2ad14376037d7089d5f005a7d283c57ba27bb0dc
itsnoctural <85783226+itsnoctural@users.noreply.github.com>
2024-10-08 22:25:06 +0300

    fix(web): disable tanstack query refetch on client

--- apps/web/src/components/client-providers.tsx
+++ apps/web/src/components/client-providers.tsx
@@ -10,9 +10,9 @@ export function ClientProviders({ children }: { children: React.ReactNode }) {
       new QueryClient({
         defaultOptions: {
           queries: {
-            // With SSR, we usually want to set some default staleTime
-            // above 0 to avoid refetching immediately on the client
-            staleTime: 60 * 1000,
+            refetchOnMount: false,
+            refetchOnReconnect: false,
+            refetchOnWindowFocus: false,
           },
         },
       }),
