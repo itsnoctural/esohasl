a10ca4a2e9475987731491012ce39ed07b98b919
itsnoctural <85783226+itsnoctural@users.noreply.github.com>
2024-10-08 22:23:50 +0300

    fix(web): accessible for screen reader error

--- apps/web/src/components/ui/command.tsx
+++ apps/web/src/components/ui/command.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import type { DialogProps } from "@radix-ui/react-dialog";
+import { type DialogProps, DialogTitle } from "@radix-ui/react-dialog";
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import { Command as CommandPrimitive } from "cmdk";
 
@@ -28,7 +28,11 @@ interface CommandDialogProps extends DialogProps {}
 const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
   return (
     <Dialog {...props}>
-      <DialogContent className="overflow-hidden p-0">
+      <DialogContent
+        className="overflow-hidden p-0"
+        aria-describedby={undefined}
+      >
+        <DialogTitle className="sr-only" />
         <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           {children}
         </Command>
