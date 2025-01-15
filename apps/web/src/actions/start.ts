6c839bd33ccf165fe5e89e7528eb958d9f2823ac
itsnoctural <85783226+itsnoctural@users.noreply.github.com>
2025-01-15 17:26:00 +0000

    fix(web): api can't get cookie

--- apps/web/src/actions/start.ts
+++ apps/web/src/actions/start.ts
@@ -4,7 +4,9 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function start(href: string) {
-  cookies().set("esohasl.return", href);
+  cookies().set("esohasl.return", href, {
+    domain: ".esohasl.net",
+  });
 
   return redirect("https://workink.net/2vP/m5vhhukb");
 }
