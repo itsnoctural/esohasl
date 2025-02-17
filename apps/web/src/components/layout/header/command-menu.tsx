8f5c22ee6212a57e161165b3c9e00244acf79383
itsnoctural <85783226+itsnoctural@users.noreply.github.com>
2025-02-17 16:41:56 +0000

    fix(web): track searching

--- apps/web/src/components/layout/header/command-menu.tsx
+++ apps/web/src/components/layout/header/command-menu.tsx
@@ -81,9 +81,13 @@ export function CommandMenu() {
       <Button
         variant="outline"
         className={
-          "relative hidden h-8 w-full justify-start rounded-[0.5rem] bg-muted/50 text-sm font-normal text-muted-foreground shadow-none sm:inline-flex sm:pr-12 plausible-event-name=search"
+          "relative hidden h-8 w-full justify-start rounded-[0.5rem] bg-muted/50 text-sm font-normal text-muted-foreground shadow-none sm:inline-flex sm:pr-12"
         }
-        onClick={() => setOpen(true)}
+        onClick={() => {
+          // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+          (window as any).plausible("search");
+          setOpen(true);
+        }}
       >
         <span>Search the best scripts...</span>
         <kbd className="pointer-events-none absolute right-[0.3rem] top-[0.3rem] h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100">
@@ -93,8 +97,12 @@ export function CommandMenu() {
       <Button
         size="icon"
         variant={"outline"}
-        className="inline-flex sm:hidden plausible-event-name=search"
-        onClick={() => setOpen(true)}
+        className="inline-flex sm:hidden"
+        onClick={() => {
+          // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+          (window as any).plausible("search");
+          setOpen(true);
+        }}
       >
         <Search size={16} />
       </Button>
